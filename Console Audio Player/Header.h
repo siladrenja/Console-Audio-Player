64b08eb899e45fc4e1237dfa2799f972b081b882
sila drenja <matej.drenjancevic@gmail.com>
2022-02-04 16:55:05 +0100

    Brought project to a working state

---
+++ Console Audio Player/Header.h
@@ -0,0 +1,119 @@
+#pragma once
+#include <string>
+#include <filesystem>
+
+
+std::string Extensions[] = { ".wav", ".mp3", ".mp4" };
+std::wstring wExtensions[] = { L".wav", L".mp3", L".mp4" };
+
+bool hasEnding(std::string const& fullString, std::string const& ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+bool hasEnding(std::wstring const& fullString, std::wstring const& ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+bool isAudio(std::string path) {
+    for (int i = 0; i < sizeof(Extensions) / sizeof(Extensions[0]); i++) {
+        if (hasEnding(path, Extensions[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isAudio(std::wstring path) {
+    for (int i = 0; i < sizeof(Extensions) / sizeof(Extensions[0]); i++) {
+        if (hasEnding(path, wExtensions[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isFile(std::string path) {
+    for (int i = path.length() - 1; i >= 0; i--) {
+        if (path[i] == '.') {
+            return true;
+        } else if (path[i] == '\\' || path[i] == '/') {
+            return false;
+        }
+    }
+    return false;
+}
+
+bool isFile(std::wstring path) {
+    for (int i = path.length() - 1; i >= 0; i--) {
+        if (path[i] == '.') {
+            return true;
+        } else if (path[i] == '\\' || path[i] == '/') {
+            return false;
+        }
+    }
+    return false;
+}
+
+bool invalidChar(char c) {
+    return !(c >= 0 && c < 128);
+}
+void stripUnicode(std::string& str) {
+    str.erase(remove_if(str.begin(), str.end(), invalidChar), str.end());
+}
+
+
+
+
+
+
+
+bool hasEnding(std::filesystem::path const& fullString, std::filesystem::path const& ending) {
+    if (fullString.generic_wstring().length() >= ending.generic_wstring().length()) {
+        return (0 == fullString.generic_wstring().compare(fullString.generic_wstring().length() - ending.generic_wstring().length(), ending.generic_wstring().length(), ending));
+    } else {
+        return false;
+    }
+}
+
+bool isAudio(std::filesystem::path path) {
+    for (int i = 0; i < sizeof(Extensions) / sizeof(Extensions[0]); i++) {
+        if (hasEnding(path, Extensions[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isFile(std::filesystem::path path) {
+    for (int i = path.wstring().length() - 1; i >= 0; i--) {
+        if (path.wstring()[i] == '.') {
+            return true;
+        } else if (path.wstring()[i] == '\\' || path.wstring()[i] == '/') {
+            return false;
+        }
+    }
+    return false;
+}
+
+std::string tostringnoUTF(std::filesystem::path path) {
+    std::wstring temp = path.wstring();
+    std::string out;
+    for (int i = 0; i < temp.length(); i++) {
+        if (static_cast<unsigned char>(temp[i]) > 127) {
+            out.push_back('_');
+        } else {
+            out.push_back(temp[i]);
+        }
+    }
+
+    return out;
+}
+
---
+++ Console Audio Player/Source.cpp
@@ -0,0 +1,299 @@
+#include "soloud.h"
+#include "soloud_wav.h"
+#include "soloud_thread.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <filesystem>
+#include "Header.h"
+#include <thread>
+#include <algorithm>
+#include <random>
+
+
+
+using namespace std;
+
+
+string Location = ".\\";
+
+void Player();
+thread t1(Player);
+
+SoLoud::Soloud soloud;
+SoLoud::Wav _sample;
+
+
+int CurrentAudio;
+
+vector<filesystem::path> SongQueue;
+
+
+#pragma region Commands
+int ping(string a) {
+	cout << "pong";
+	return 0;
+}
+
+int move(string a) {
+	if (filesystem::exists(a));
+	if (a[1] == ':') {
+		if (!filesystem::exists(a)){
+			cout << "invalid path" << endl;
+			return -1;
+		}
+		Location = a;
+	} else {
+		if (a[0] == '\\') {
+
+			if (!filesystem::exists(Location + (a.substr(1, a.length() - 1)))) {
+				cout << "invalid path" << endl;
+				return -1;
+			}
+
+			Location.append(a.substr(1, a.length()-1));
+		} else {
+
+			if (!filesystem::exists(Location + a)) {
+				cout << "invalid path" << endl;
+				return -1;
+			}
+
+			Location.append(a);
+		}
+	}
+
+	if (Location[Location.length() - 1] != '\\') {
+		Location.push_back('\\');
+	}
+
+	return 0;
+}
+
+int leave(string a) {
+	
+	exit(0);
+}
+
+int dir(string a) {
+
+	system((char*)(("dir " + a + " /b").c_str()));
+
+	return 0;
+}
+
+int tree(string a) {
+	system("tree");
+	return 0;
+}
+
+int play(string a) {
+	if (!a.empty()) {
+		if (filesystem::exists(a));
+		if (a[1] == ':') {
+			if (!filesystem::exists(a)) {
+				cout << "invalid path" << endl;
+				return -1;
+			}
+			if (isFile(a)) {
+				if (isAudio(a)) {
+					SongQueue.push_back(filesystem::absolute(a).string());
+				} else {
+					cout << "invalid file type" << endl;
+				}
+			} else {
+				for (filesystem::path p : filesystem::directory_iterator(a)) {
+					if (isAudio(p.generic_string())) {
+						SongQueue.push_back(p.string());
+					}
+				}
+			}
+		} else {
+			if (a[0] == '\\') {
+
+				if (!filesystem::exists(Location + (a.substr(1, a.length() - 1)))) {
+					cout << "invalid path" << endl;
+					return -1;
+				}
+
+				if (isFile(a)) {
+					if (isAudio(a)) {
+						SongQueue.push_back(filesystem::absolute((Location + (a.substr(1, a.length() - 1)))).string());
+					} else {
+						cout << "invalid file type" << endl;
+					}
+				} else {
+					for (filesystem::path p : filesystem::directory_iterator(Location + (Location + (a.substr(1, a.length() - 1))))) {
+						if (isAudio(p.generic_string())) {
+							SongQueue.push_back(p.string());
+						}
+					}
+				}
+			} else {
+
+				if (!filesystem::exists(Location + a)) {
+					cout << "invalid path" << endl;
+					return -1;
+				}
+
+				if (isFile(a)) {
+					if (isAudio(a)) {
+						SongQueue.push_back(filesystem::absolute((Location + a)).string());
+					} else {
+						cout << "invalid file type" << endl;
+					}
+				} else {
+					for (filesystem::path p : filesystem::directory_iterator(Location + (Location + a))) {
+						if (isAudio(p.generic_string())) {
+							SongQueue.push_back(p.string());
+						}
+					}
+				}
+			}
+		}
+	} else {
+		try {
+			for (filesystem::path p : filesystem::directory_iterator(Location)) {
+
+				//filesystem::rename(p, stripUnicode(p));
+				//cout << tostringnoUTF(p) << endl;
+				if (isAudio(p)) {
+					SongQueue.push_back(p);
+				}
+			}
+		} catch (const std::exception& exc) {
+			cout << exc.what() << endl;
+		}
+	}
+
+
+	return 0;
+}
+
+int Queue(string a) {
+	for (filesystem::path q : SongQueue) {
+		cout << tostringnoUTF(q) << endl;
+	}
+
+	return 0;
+}
+
+int skip(string a) {
+	soloud.stopAll();
+	return 0;
+}
+
+int Volume(string a) {
+	soloud.setGlobalVolume(stof(a));
+	
+	return 0;
+}
+
+int stop(string a) {
+	SongQueue.clear();
+	soloud.stopAll();
+	return 0;
+}
+
+int _Shuffle(string a) {
+	auto rng = std::default_random_engine{};
+	std::shuffle(std::begin(SongQueue), std::end(SongQueue), rng);
+	return 0;
+}
+
+int _System(string a) {
+	system(a.c_str());
+	return 0;
+}
+
+int _cls(string a) {
+	system("cls");
+	return 0;
+}
+
+#pragma endregion
+
+
+
+
+string Commands[] = { "ping", "cd", "exit", "quit", "leave", "dir", "tree","play", "queue", "skip", "volume", "v", "stop", "shuffle", "system", "cls"};
+int ((*CommandFunctions[])(string)) = {ping, move, leave, leave , leave, dir, tree, play, Queue, skip, Volume, Volume, stop, _Shuffle, _System, _cls};
+
+void Player() {
+
+
+	soloud.init();
+	while (true) {
+		if (!SongQueue.empty()) {
+			CurrentAudio = _sample.load(SongQueue[0].wstring().c_str());
+			soloud.play(_sample);
+
+			SongQueue.erase(SongQueue.begin());
+		}
+
+		while (soloud.getActiveVoiceCount() > 0) {
+			SoLoud::Thread::sleep(100);
+		}
+	}
+
+	soloud.deinit();
+}
+
+int main(int argc, char* argv[]) {
+
+	
+	int spacePos;
+
+	string Command;
+
+	
+
+	cout << filesystem::absolute(Location) << ">>";
+	getline(cin, Command);
+	
+
+	for (int i = 0; i < Command.size() && Command[i] != ' '; i++)spacePos = i+1;
+	
+	for (int i = 0; i < sizeof(Commands)/sizeof(Commands[0]); i++) {
+		if (Command.substr(0, spacePos) == Commands[i]) {
+			Command.erase(0, spacePos);
+			if (!Command.empty()) {
+				if (Command[0] == ' ') {
+					Command.erase(0, 1);
+				}
+			}
+			(*CommandFunctions[i])(Command);
+		}
+	}
+
+	
+	
+	
+
+
+
+	while (true) {
+		cout << filesystem::absolute(Location) << ">>";
+		getline(cin, Command);
+
+		for (int i = 0; i < Command.size() && Command[i] != ' '; i++)spacePos = i + 1;
+
+		for (int i = 0; i < sizeof(Commands) / sizeof(Commands[0]); i++) {
+			if (Command.substr(0, spacePos) == Commands[i]) {
+				Command.erase(0, spacePos);
+				if (!Command.empty()) {
+					if (Command[0] == ' ') {
+						Command.erase(0, 1);
+					}
+				}
+				(*CommandFunctions[i])(Command);
+			}
+		}
+		
+	}
+
+	t1.join();
+
+	return 0;
+}
